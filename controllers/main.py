a06709aa211310033d0a4e081fbe62879f7214b3
onlyone-odoo <matiasb@onlyone.com.ar>
2025-08-07 14:36:47 -0300

    refactor a cart

--- controllers/main.py
+++ controllers/main.py
@@ -3,25 +3,34 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
 class WebsiteSaleCustom(WebsiteSale):
-    @http.route(
-        ["/shop/address"],
-        type="http",
-        methods=["GET", "POST"],
-        auth="public",
-        website=True,
-        sitemap=False,
-    )
-    def address(self, **kw):
-        response = super(WebsiteSaleCustom, self).address(**kw)
-        values = response.qcontext  # Accede a los valores que se pasan a la plantilla
-
+    @http.route(["/shop/cart"], type="http", auth="public", website=True, sitemap=False)
+    def cart(self, access_token=None, revive="", **post):
+        response = super(WebsiteSaleCustom, self).cart(access_token, revive, **post)
         order = http.request.website.sale_get_order()
         if order and order.partner_id:
-            # Obtén todos los contactos hijos del partner actual (usuario activo)
             child_contacts = order.partner_id.child_ids.sudo().filtered(
-                lambda p: p.type in ("delivery", "contact") and p.active
+                lambda p: p.type in ("contact", "delivery") and p.active
             )
-            # Reemplaza o une con la lista existente de direcciones (asumiendo key 'addresses')
-            values["addresses"] = child_contacts
+            response.qcontext["child_contacts"] = child_contacts
+        return response
 
-        return http.request.render("website_sale.address", values)
+    @http.route(
+        ["/shop/select_child"],
+        type="http",
+        auth="public",
+        website=True,
+        methods=["POST"],
+    )
+    def select_child(self, child_id=None, **post):
+        order = http.request.website.sale_get_order(force_create=True)
+        if child_id:
+            child = http.request.env["res.partner"].sudo().browse(int(child_id))
+            if (
+                child
+                and child.parent_id == order.partner_id
+                and child.type in ("contact", "delivery")
+            ):
+                order.partner_shipping_id = child
+        else:
+            order.partner_shipping_id = order.partner_id  # Reset a "yo mismo"
+        return http.request.redirect("/shop/cart")
