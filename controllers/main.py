995ea6d96ee7e25bd55c7405642358c7096b5724
onlyone-odoo <matiasb@onlyone.com.ar>
2025-08-07 14:48:25 -0300

    loggers

--- controllers/main.py
+++ controllers/main.py
@@ -1,5 +1,8 @@
 from odoo import http
 from odoo.addons.website_sale.controllers.main import WebsiteSale
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class WebsiteSaleCustom(WebsiteSale):
@@ -7,11 +10,17 @@ class WebsiteSaleCustom(WebsiteSale):
     def cart(self, access_token=None, revive="", **post):
         response = super(WebsiteSaleCustom, self).cart(access_token, revive, **post)
         order = http.request.website.sale_get_order()
+        child_contacts = http.request.env["res.partner"]
         if order and order.partner_id:
+            _logger.info("Order Partner ID: %s", order.partner_id.id)
             child_contacts = order.partner_id.child_ids.sudo().filtered(
                 lambda p: p.type in ("contact", "delivery") and p.active
             )
+            _logger.info("Child Contacts: %s", child_contacts)
             response.qcontext["child_contacts"] = child_contacts
+        else:
+            _logger.warning("No order or partner found in cart!")
+        _logger.info("QContext keys: %s", response.qcontext.keys())
         return response
 
     @http.route(
@@ -31,6 +40,8 @@ class WebsiteSaleCustom(WebsiteSale):
                 and child.type in ("contact", "delivery")
             ):
                 order.partner_shipping_id = child
+                _logger.info("Selected shipping partner: %s", child.name)
         else:
-            order.partner_shipping_id = order.partner_id  # Reset a "yo mismo"
+            order.partner_shipping_id = order.partner_id
+            _logger.info("Reset to main partner: %s", order.partner_id.name)
         return http.request.redirect("/shop/cart")
