86b46e5fb0a322a9c1164b60a6c4c62e2692cc8c
onlyone-odoo <matiasb@onlyone.com.ar>
2025-08-08 12:02:58 -0300

    edit JS

--- static/src/js/child_select.js
+++ static/src/js/child_select.js
@@ -2,12 +2,19 @@ $(document).ready(function() {
     $('#child_select').on('change', function () {
         var childId = $(this).val();
         console.log('Changing to child_id:', childId);
-        $.post('/shop/select_child', { child_id: childId }).done(function (response) {
-            console.log('Success:', response);
-            location.reload(); // Refresca para ver el cambio
-        }).fail(function (jqXHR, textStatus, errorThrown) {
-            console.error('Fail:', textStatus, errorThrown);
-            alert('Error: ' + textStatus);
+        $.ajax({
+            url: '/shop/select_child',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ child_id: childId }),
+            success: function(response) {
+                console.log('Success:', response);
+                location.reload();
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error('Fail:', textStatus, errorThrown);
+                alert('Error: ' + textStatus);
+            }
         });
     });
 });
